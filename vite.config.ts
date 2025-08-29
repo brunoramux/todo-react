924a296b9390bb5cabccc6da5594ce570aaa8ed2
Bruno Ramos Lemos <bruno.lemos@live.com>
2025-08-28 21:48:56 -0300

    Icon component

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+// Plugin para importar SVGs como componentes React
+import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
+// https://vite.dev/config/ 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
 })
