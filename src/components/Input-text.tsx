b99f88e8df822418021d6b111ca78a1103c60ee5
Bruno Ramos Lemos <bruno.lemos@live.com>
2025-09-01 19:26:46 -0300

    Input text Component

---
+++ src/components/Input-text.tsx
@@ -0,0 +1,31 @@
+import { cva, cx, type VariantProps } from "class-variance-authority";
+import type React from "react";
+import { textVariants } from "./Text";
+
+export const inputTextVariants = cva("border-b border-solid border-gray-200 focus:border-pink-base", {
+    variants: {
+        size: {
+            md: "pb-2 px-2"
+        },
+        disabled: {
+            true: "pointer-events-none"
+        }
+    },
+    defaultVariants: {
+        size: "md",
+        disabled: false
+    }
+})
+
+interface InputTextProps extends VariantProps<typeof inputTextVariants>,
+    Omit<React.ComponentProps<"input">, "size" | "disabled"> {} 
+
+
+export default function InputText({ size, disabled, className, ...props}: InputTextProps){
+    return (
+        <input 
+            className={cx(inputTextVariants({ size, disabled, className }), textVariants(), className)}
+            {...props} 
+        />
+    )
+}
